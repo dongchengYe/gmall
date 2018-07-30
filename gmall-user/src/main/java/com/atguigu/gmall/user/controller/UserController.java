6b6e518a619c52319242897917731b0ecd5277d6
ydc <laoye.1@qq.com>
2018-07-30 22:20:40 +0800

    Second commit ,create user Service.

---
+++ gmall-user/src/main/java/com/atguigu/gmall/user/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.atguigu.gmall.user.controller;
+
+import com.atguigu.gmall.user.bean.UserInfo;
+import com.atguigu.gmall.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @ResponseBody
+    @RequestMapping(value="userInfoList")
+    public List<UserInfo> userInfoList(){
+        return userService.queryUserInfoList();
+    }
+}
