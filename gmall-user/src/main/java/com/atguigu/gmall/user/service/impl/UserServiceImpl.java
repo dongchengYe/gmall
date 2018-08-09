2f873b391cfb85b166d124f96222e570957377f8
ydc <laoye.1@qq.com>
2018-08-09 18:48:19 +0800

    Third commit ,spu manage function is completed.

--- gmall-user/src/main/java/com/atguigu/gmall/user/service/impl/UserServiceImpl.java
+++ gmall-user/src/main/java/com/atguigu/gmall/user/service/impl/UserServiceImpl.java
@@ -1,10 +1,13 @@
 package com.atguigu.gmall.user.service.impl;
 
+import com.alibaba.dubbo.config.annotation.Service;
+import com.atguigu.gmall.bean.UserAddress;
 import com.atguigu.gmall.bean.UserInfo;
 import com.atguigu.gmall.service.UserService;
+import com.atguigu.gmall.user.mapper.UserAddressMapper;
 import com.atguigu.gmall.user.mapper.UserInfoMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+
 
 import java.util.List;
 
@@ -14,8 +17,15 @@ public class UserServiceImpl implements UserService {
     @Autowired
     UserInfoMapper userInfoMapper;
 
+    @Autowired
+    UserAddressMapper userAddressMapper;
+
     public List<UserInfo> queryUserInfoList(){
 
         return userInfoMapper.selectAll();
+    }
+
+    public List<UserAddress> queryUserAddressList(){
+        return userAddressMapper.selectAll();
     }
 }
